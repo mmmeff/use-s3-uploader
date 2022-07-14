f7bf001dab9a79cae7b4a59d876df3638d85414a
Matthew Frey <mfrey@stokespace.com>
2022-07-14 16:06:17 -0700

    export options type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import useS3Uploader from './useS3Uploader';
+import useS3Uploader, { Options } from './useS3Uploader';
+
 export default useS3Uploader;
-export { useS3Uploader };
+export { useS3Uploader, Options as S3UploaderOptions };
+
 
 
